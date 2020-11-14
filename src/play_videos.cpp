c3646ac7dd1f36b0499fc00f8541fcb217dd99a9
Nguyen Cong Sang <cs.ncsang@gmail.com>
2020-11-14 21:48:00 +0700

    qcm

--- src/play_videos.cpp
+++ src/play_videos.cpp
@@ -3,7 +3,159 @@
 #include <sstream>
 #include <string>
 
-// using namespace cv;
+struct Pixel
+{
+    float column;
+    float row;
+    Pixel(const float column = 0, const float row = 0) : column(column), row(row)
+    {
+    }
+};
+
+int get_value(int point_0, int point_1, int point_2, int point_3, float x)
+{
+    float a = -0.5 * point_0 + 1.5 * point_1 - 1.5 * point_2 + 0.5 * point_3;
+    float b = point_0 - 2.5 * point_1 + 2 * point_2 - 0.5 * point_3;
+    float c = -0.5 * point_0 + 0.5 * point_2;
+    float d = point_1;
+    int value = (int)std::floor(d + x * (c + x * (b + x * a)));
+    if (value < 0)
+    {
+        value = 0;
+    }
+    else if (value > 255)
+    {
+        value = 255;
+    }
+    return value;
+}
+
+void resize_bicubic(cv::Mat& input_image, cv::Mat& output_image, const cv::Size& size)
+{
+    std::vector<Pixel> pixel_map;
+    size_t map_size = size.width * size.height;
+    pixel_map.reserve(map_size);
+    for (size_t i = 0; i < map_size; ++i)
+    {
+        int column_new = i % size.width;
+        int row_new = i / size.width;
+        float column = ((float)(input_image.cols) / (size.width) * (column_new + 0.5)) - 0.5;
+        float row = ((float)(input_image.rows) / (size.height) * (row_new + 0.5)) - 0.5;
+        pixel_map.emplace_back(Pixel(column, row));
+    }
+    int number_of_channels = input_image.channels();
+    int image_type = input_image.type();
+    output_image.create(size, image_type);
+
+    for (int row_output = 0; row_output < output_image.cols; ++row_output)
+    {
+        for (int col_output = 0; col_output < output_image.rows; ++col_output)
+        {
+            int output_index = row_output * output_image.cols + col_output;
+            float input_image_row = pixel_map[output_index].row;
+            float input_image_col = pixel_map[output_index].column;
+            int input_x0, input_x1, input_x2, input_x3;
+            if (std::floor(input_image_col) == -1)
+            {
+                input_x0 = 0;
+                input_x1 = 0;
+                input_x2 = 0;
+                input_x3 = 1;
+            }
+            else if (std::floor(input_image_col) == 0)
+            {
+                input_x0 = 0;
+                input_x1 = 0;
+                input_x2 = 1;
+                input_x3 = 2;
+            }
+            else if (std::floor(input_image_col) == input_image.cols - 2)
+            {
+                input_x0 = input_image.cols - 3;
+                input_x1 = input_image.cols - 2;
+                input_x2 = input_image.cols - 1;
+                input_x3 = input_image.cols - 1;
+            }
+            else if (std::floor(input_image_col) == input_image.cols - 1)
+            {
+                input_x0 = input_image.cols - 2;
+                input_x1 = input_image.cols - 1;
+                input_x2 = input_image.cols - 1;
+                input_x3 = input_image.cols - 1;
+            }
+            else
+            {
+                input_x0 = std::floor(input_image_col) - 1;
+                input_x1 = input_x0 + 1;
+                input_x2 = input_x0 + 2;
+                input_x3 = input_x0 + 3;
+            }
+            int input_y0, input_y1, input_y2, input_y3;
+            if (std::floor(input_image_row) == -1)
+            {
+                input_y0 = 0;
+                input_y1 = 0;
+                input_y2 = 0;
+                input_y3 = 1;
+            }
+            else if (std::floor(input_image_row) == 0)
+            {
+                input_y0 = 0;
+                input_y1 = 0;
+                input_y2 = 1;
+                input_y3 = 2;
+            }
+            else if (std::floor(input_image_row) == input_image.rows - 2)
+            {
+                input_y0 = input_image.rows - 3;
+                input_y1 = input_image.rows - 2;
+                input_y2 = input_image.rows - 1;
+                input_y3 = input_image.rows - 1;
+            }
+            else if (std::floor(input_image_row) == input_image.rows - 1)
+            {
+                input_y0 = input_image.rows - 2;
+                input_y1 = input_image.rows - 1;
+                input_y2 = input_image.rows - 1;
+                input_y3 = input_image.rows - 1;
+            }
+            else
+            {
+                input_y0 = std::floor(input_image_row) - 1;
+                input_y1 = input_y0 + 1;
+                input_y2 = input_y0 + 2;
+                input_y3 = input_y0 + 3;
+            }
+            float d_x = input_image_col - std::floor(input_image_col);
+            float d_y = input_image_row - std::floor(input_image_row);
+            for (int i = 0; i < number_of_channels; ++i)
+            {
+                uchar source_x_y_0 = get_value(input_image.data[(input_y0 * input_image.cols + input_x0) * number_of_channels + i],
+                                               input_image.data[(input_y0 * input_image.cols + input_x1) * number_of_channels + i],
+                                               input_image.data[(input_y0 * input_image.cols + input_x2) * number_of_channels + i],
+                                               input_image.data[(input_y0 * input_image.cols + input_x3) * number_of_channels + i],
+                                               d_x);
+                uchar source_x_y_1 = get_value(input_image.data[(input_y1 * input_image.cols + input_x0) * number_of_channels + i],
+                                               input_image.data[(input_y1 * input_image.cols + input_x1) * number_of_channels + i],
+                                               input_image.data[(input_y1 * input_image.cols + input_x2) * number_of_channels + i],
+                                               input_image.data[(input_y1 * input_image.cols + input_x3) * number_of_channels + i],
+                                               d_x);
+                uchar source_x_y_2 = get_value(input_image.data[(input_y2 * input_image.cols + input_x0) * number_of_channels + i],
+                                               input_image.data[(input_y2 * input_image.cols + input_x1) * number_of_channels + i],
+                                               input_image.data[(input_y2 * input_image.cols + input_x2) * number_of_channels + i],
+                                               input_image.data[(input_y2 * input_image.cols + input_x3) * number_of_channels + i],
+                                               d_x);
+                uchar source_x_y_3 = get_value(input_image.data[(input_y3 * input_image.cols + input_x0) * number_of_channels + i],
+                                               input_image.data[(input_y3 * input_image.cols + input_x1) * number_of_channels + i],
+                                               input_image.data[(input_y3 * input_image.cols + input_x2) * number_of_channels + i],
+                                               input_image.data[(input_y3 * input_image.cols + input_x3) * number_of_channels + i],
+                                               d_x);
+                uchar source_value = get_value(source_x_y_0, source_x_y_1, source_x_y_2, source_x_y_3, d_y);
+                output_image.data[output_index * number_of_channels + i] = source_value;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -14,11 +166,30 @@ int main()
         std::cin.get();
         return -1;
     }
-    double fps = cap.get(cv::CAP_PROP_FPS);
-    std::cout << "Frames per seconds : " << fps << std::endl;
-    cv::String window_name = "My First Video";
-    namedWindow(window_name, cv::WINDOW_NORMAL);
-    int frame_count = 0;
+    float ratio;
+    do
+    {
+        std::cout << "enter ratio: " << std::endl;
+        std::cin >> ratio;
+    } while (ratio <= 0);
+    
+    int frame_width = static_cast<int>(cap.get(3));
+    int frame_height = static_cast<int>(cap.get(4));
+    cv::Size frame_size(frame_width * ratio, frame_height * ratio);
+    int frames_per_second = 1;
+    cv::VideoWriter oVideoWriter("/home/nq/workspace/image_interpolation/MyVideo.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
+                                 frames_per_second, frame_size, true);
+    if (oVideoWriter.isOpened() == false)
+    {
+        std::cout << "Cannot save the video to a file" << std::endl;
+        std::cin.get(); //wait for any key press
+        return -1;
+    };
+    // double fps = cap.get(cv::CAP_PROP_FPS);
+    // std::cout << "Frames per seconds : " << fps << std::endl;
+    // cv::String window_name = "My First Video";
+    // namedWindow(window_name, cv::WINDOW_NORMAL);
+    // int frame_count = 0;
     while (true)
     {
         cv::Mat frame;
@@ -28,23 +199,29 @@ int main()
             std::cout << "Found the end of the video" << std::endl;
             break;
         }
-        ++frame_count;
-        std::string ss_frame_count = std::to_string(frame_count);
-        cv::putText(frame, ss_frame_count, cv::Point(25, 25), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, cv::Scalar(0, 0, 255), 1, CV_AA);
-        cv::Size sz = frame.size();
-        int imageWith = sz.width;
-        std::string image_With = std::to_string(imageWith);
-        int imageHigh = sz.height;
-        std::string image_High = std::to_string(imageHigh);
-        std::string multiple = "x";
-        std::string with_high = image_With + multiple + image_High;
-       cv::putText(frame, with_high, cv::Point(imageWith - 100, imageHigh - 40), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, cv::Scalar(0, 0, 255), 1, CV_AA);
-        imshow(window_name, frame);
+        cv::Mat new_frame;
+        cv::Size new_size(frame.cols * ratio, frame.rows * ratio);
+        resize_bicubic(frame, new_frame, new_size);
+        oVideoWriter.write(new_frame); 
+        imshow("new_frame", new_frame);
+        // ++frame_count;
+        // std::string ss_frame_count = std::to_string(frame_count);
+        // cv::putText(frame, ss_frame_count, cv::Point(25, 25), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, cv::Scalar(0, 0, 255), 1, CV_AA);
+        // cv::Size sz = frame.size();
+        // int imageWith = sz.width;
+        // std::string image_With = std::to_string(imageWith);
+        // int imageHigh = sz.height;
+        // std::string image_High = std::to_string(imageHigh);
+        // std::string multiple = "x";
+        // std::string with_high = image_With + multiple + image_High;
+        // cv::putText(frame, with_high, cv::Point(imageWith - 100, imageHigh - 40), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, cv::Scalar(0, 0, 255), 1, CV_AA);
+        // imshow(window_name, frame);
         if (cv::waitKey(10) == 27)
         {
             std::cout << "Esc key is pressed by user. Stoppig the video" << std::endl;
             break;
         }
     }
+
     return 0;
 }
--- src/rotate_image_bicubic.cpp
+++ src/rotate_image_bicubic.cpp
@@ -33,6 +33,26 @@ int get_value(int point_0, int point_1, int point_2, int point_3, float x)
     }
     return value;
 }
+
+cv::Point2f rotate(cv::Point2f A, float angle)
+{
+    float rad = (angle * M_PI) / 180;
+    cv::Point2f B;
+    B.x = A.x * cos(rad) - A.y * sin(rad);
+    B.y = A.x * sin(rad) + A.y * cos(rad);
+    return B;
+}
+cv::Point2f rotate_AX(cv::Point2f A, cv::Point2f X, float angle)
+{
+    cv::Point2f A1;
+    A1.x = A.x - X.x;
+    A1.y = A.y - X.y;
+    cv::Point2f P = rotate(A1, angle);
+    P.x += X.x;
+    P.y += X.y;
+    return P;
+}
+
 float get_euclid_distance(cv::Point2f point_1, cv::Point2f point_2)
 {
     float d_x = point_1.x - point_2.x;
@@ -48,43 +68,106 @@ void rotate_image(cv::Mat& input_image, cv::Mat& output_image, float angle)
     pixel_map.reserve(map_size);
     std::cout << "map_size: " << map_size << std::endl;
 
-    cv::Point2f center;
-    center.x = (float)input_image.cols / 2;
-    center.y = (float)input_image.rows / 2;
-    std::cout << "center_x: " << center.x << std::endl;
-    std::cout << "center_y: " << center.y << std::endl;
-    float rad = ((angle)*M_PI) / 180;
-    
-    cv::Point2f point_input;
-    point_input.x = input_image.cols;
-    point_input.y = input_image.rows;
+    // cv::Point2f center;
+    // center.x = (float)input_image.cols / 2;
+    // center.y = (float)input_image.rows / 2;
+    // std::cout << "center_x: " << center.x << std::endl;
+    // std::cout << "center_y: " << center.y << std::endl;
+    // float rad = ((angle)*M_PI) / 180;
 
-    float distant_input = get_euclid_distance(center, point_input);
-    int radius_input = std::round(distant_input);
-    std::cout << "radius_input: " << radius_input << std::endl;
+    // cv::Point2f point_input;
+    // point_input.x = input_image.cols;
+    // point_input.y = input_image.rows;
 
-    cv::Size size(radius_input * 2, radius_input * 2);
+    // float distant_input = get_euclid_distance(center, point_input);
+    // int radius_input = std::round(distant_input);
+    // std::cout << "radius_input: " << radius_input << std::endl;
+
+    // cv::Size size(radius_input * 2, radius_input * 2);
+    // int number_of_channels = input_image.channels();
+    // int image_type = input_image.type();
+    // output_image.create(size, image_type);
+
+    // cv::Point2f center_output;
+    // center_output.x = (float)output_image.cols / 2;
+    // center_output.y = (float)output_image.rows / 2;
+
+    // cv::Point2f point_change;
+    // point_change.x = center_output.x - center.x;;
+    // point_change.y = center_output.y - center.y;
+    cv::Point2f input_center;
+    input_center.x = input_image.cols / 2;
+    input_center.y = input_image.rows / 2;
+
+    cv::Point2f a, b, c, d;
+    a.x = input_image.cols;
+    a.y = 0;
+    b.x = 0;
+    b.y = 0;
+    c.x = 0;
+    c.y = input_image.rows;
+    d.x = input_image.cols;
+    d.y = input_image.rows;
+
+    cv::Point2f a_1, b_1, c_1, d_1;
+    a_1 = rotate_AX(a, input_center, angle);
+    b_1 = rotate_AX(b, input_center, angle);
+    c_1 = rotate_AX(c, input_center, angle);
+    d_1 = rotate_AX(d, input_center, angle);
+
+    std::vector<float> point_x;
+    std::vector<float> point_y;
+    point_x.push_back(a_1.x);
+    point_x.push_back(b_1.x);
+    point_x.push_back(c_1.x);
+    point_x.push_back(d_1.x);
+
+    point_y.push_back(a_1.y);
+    point_y.push_back(b_1.y);
+    point_y.push_back(c_1.y);
+    point_y.push_back(d_1.y);
+    std::sort(point_x.begin(), point_x.end());
+    std::sort(point_y.begin(), point_y.end());
+
+    int size = point_x.size();
+    int d_x, d_y;
+    d_x = std::round(point_x[size - 1] - point_x[0]);
+    d_y = std::round(point_y[size - 1] - point_y[0]);
+
     int number_of_channels = input_image.channels();
     int image_type = input_image.type();
-    output_image.create(size, image_type);
-
+    // output_image.create(d_y, d_x, image_type);
+    output_image = cv::Mat::zeros(d_y, d_x, image_type);
+    
     cv::Point2f center_output;
     center_output.x = (float)output_image.cols / 2;
     center_output.y = (float)output_image.rows / 2;
 
     cv::Point2f point_change;
-    point_change.x = center_output.x - center.x;;
-    point_change.y = center_output.y - center.y;
-    
+    point_change.x = center_output.x - input_center.x;
+    point_change.y = center_output.y - input_center.y;
+
     for (int row_output = 0; row_output < output_image.rows; ++row_output)
     {
         for (int col_output = 0; col_output < output_image.cols; ++col_output)
         {
-            float column = (((col_output + 0.5) - center_output.x) * cos(rad) - ((row_output + 0.5) - center_output.y) * sin(rad)) + center_output.x;
-            float row = (((col_output + 0.5) - center_output.x) * sin(rad) + ((row_output + 0.5) - center_output.y) * cos(rad)) + center_output.y;
-            pixel_map.emplace_back(Pixel(column - point_change.x, row - point_change.y));
+            cv::Point2f output_point;
+            output_point.x = col_output + 0.5;
+            output_point.y = row_output + 0.5;
+            cv::Point2f rotate = rotate_AX(output_point, center_output, angle);
+            pixel_map.emplace_back(Pixel(rotate.x - point_change.x, rotate.y - point_change.y));
         }
     }
+
+    // for (int row_output = 0; row_output < output_image.rows; ++row_output)
+    // {
+    //     for (int col_output = 0; col_output < output_image.cols; ++col_output)
+    //     {
+    //         float column = (((col_output + 0.5) - center_output.x) * cos(rad) - ((row_output + 0.5) - center_output.y) * sin(rad)) + center_output.x;
+    //         float row = (((col_output + 0.5) - center_output.x) * sin(rad) + ((row_output + 0.5) - center_output.y) * cos(rad)) + center_output.y;
+    //         pixel_map.emplace_back(Pixel(column - point_change.x, row - point_change.y));
+    //     }
+    // }
     std::cout << "input_image_col: " << pixel_map[200].column << "; input_image.row: " << pixel_map[300].row << std::endl;
     for (int row_output = 0; row_output < output_image.cols; ++row_output)
     {
@@ -191,7 +274,7 @@ void rotate_image(cv::Mat& input_image, cv::Mat& output_image, float angle)
 
 int main()
 {
-    cv::Mat input_image = cv::imread("01.jpg");
+    cv::Mat input_image = cv::imread("01.jpg", 0);
     if (input_image.empty())
     {
         std::cout << "Could not open or find the image" << std::endl;
